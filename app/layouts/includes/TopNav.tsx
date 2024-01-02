83b6d232ecb1be62515d52da8a1c4110f458f2f7
OGONO <bobgenga3@gmail.com>
2024-01-02 19:14:02 +0200

    ...

--- app/layouts/includes/TopNav.tsx
+++ app/layouts/includes/TopNav.tsx
@@ -12,6 +12,7 @@ import useCreateBucketUrl from "@/app/hooks/useCreateBucketUrl"
 import { RandomUsers } from "@/app/types"
 import useSearchProfilesByName from "@/app/hooks/useSearchProfilesByName";
 import { Bars3Icon } from "@heroicons/react/16/solid";
+import SideNavMain from "./SideNavMain";
 
 export default function TopNav() {    
     const userContext = useUser()
@@ -48,11 +49,11 @@ export default function TopNav() {
             <div id="TopNav" className="fixed bg-white z-30 flex items-center w-full border-b h-[60px]">
                 <div className={`flex items-center justify-between gap-6 w-full px-4 mx-auto ${pathname === '/' ? 'max-w-[1150px]' : ''}`}>
                     <div>
-                     <Link href="./SideNavMain">
+                     <SideNavMain>
                         <a className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border">
                             <Bars3Icon className="h-5 w-5" />
                         </a>
-                      </Link>
+                      </SideNavMain>
                     </div>
                     <Link href="/">
                      <h1 className="font-bold text-3xl">PrideTok</h1>
