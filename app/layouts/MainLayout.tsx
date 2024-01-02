b97a801af607177e85dc15e3e1a4f32d5ddd77ee
OGONO <bobgenga3@gmail.com>
2024-01-02 19:41:21 +0200

    ...

--- app/layouts/MainLayout.tsx
+++ app/layouts/MainLayout.tsx
@@ -1,16 +1,23 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import SideNavMain from "./includes/SideNavMain"
 import TopNav from "./includes/TopNav"
 import { usePathname } from "next/navigation"
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
+	const [isSSR, setIsSSR] = useState(true);
+
+    useEffect(() => {
+    setIsSSR(false);
+     }, []);  
+
+    if (isSSR) return null;
+
     const pathname = usePathname()
 
     return (
       	<>
 			<TopNav/>
-			<div className={`flex justify-between mx-auto w-full lg:px-2.5 px-0 ${pathname == '/' ? 'max-w-[1140px]' : ''}`}>
-				
+			<div className={`flex justify-between md:gap-20 mx-auto w-full lg:px-2.5 px-0 ${pathname == '/' ? 'max-w-[1140px]' : ''}`}>
 				{children}
 			</div>
       	</>
